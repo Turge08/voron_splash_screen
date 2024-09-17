b080ea120a997263183278d4e3ca41dae17fe61c
Turge08 <Turge08@gmail.com>
2024-09-17 14:42:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ sudo apt install -y vlc
 sudo cp splash.service /etc/systemd/system/
 sudo sed -i "s/userid/$USER/" /etc/systemd/system/splash.service
 sudo systemctl enable splash.service
-cmdline=$(cat /boot/cmdline.txt)
-sudo cp /boot/cmdline.txt /boot/cmdline.txt.bak
-echo "$cmdline quiet vt.global_cursor_default=0 plymouth.ignore-serial-consoles" | sudo tee /boot/cmdline.txt
+cmdline=$(cat /boot/firmware/cmdline.txt)
+sudo cp /boot/cmdline.txt /boot/firmware/cmdline.txt.bak
+echo "$cmdline quiet vt.global_cursor_default=0 plymouth.ignore-serial-consoles" | sudo tee /boot/firmware/cmdline.txt
